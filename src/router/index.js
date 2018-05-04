4b993c7cb4b92c9f58a983229ec5738b94ee2692
viaeoo <viaeoo@gmail.com>
2018-05-05 01:17:08 +0900

    app router setting

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/pages/Home'
+import Instructors from '@/pages/Instructors'
+import HowToUse from '@/pages/HowToUse'
+import Faq from '@/pages/Faq'
+import Contact from '@/pages/Contact'
 
 Vue.use(Router)
 
@@ -12,6 +16,26 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/instructors',
+      name: 'instructors',
+      component: Instructors
+    },
+    {
+      path: '/howtouse',
+      name: 'howtouse',
+      component: HowToUse
+    },
+    {
+      path: '/faq',
+      name: 'faq',
+      component: Faq
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
     }
   ]
 })
