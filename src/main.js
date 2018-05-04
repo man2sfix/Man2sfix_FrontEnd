03bde832c25dde6141f46b24bd628acf7c943f2e
viaeoo <viaeoo@gmail.com>
2018-05-05 01:16:03 +0900

    app setting & custom

--- src/main.js
+++ src/main.js
@@ -4,12 +4,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ko'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false
+Vue.use(ElementUI, { locale })
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  render (h) {
+    return h(App)
+  }
 })
